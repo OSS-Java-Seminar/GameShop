c730a27f4efd842ab1f99e3893ce85dc8b10a4b5
Marin Rancic <ranca.skalice@gmail.com>
2022-01-16 18:52:27 +0100

    admin panel update

--- radak_rancic/src/main/java/backend/Adminpanel/AdminGameController.java
+++ radak_rancic/src/main/java/backend/Adminpanel/AdminGameController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.adminportal.domain.Laptop;
-
 import backend.service.GameService;
 import backend.service.PictureService;
 import database.entities.Game;
